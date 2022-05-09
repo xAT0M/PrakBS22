533c27f4016a358ce08c8af40f449a0b946980be
Aldo Giannuzzi <aldo.giannuzzi@smail.th-koeln.de>
2022-05-09 15:37:06 +0200

    test

--- cmake-build-docker/CMakeFiles/Makefile.cmake
+++ cmake-build-docker/CMakeFiles/Makefile.cmake
@@ -94,5 +94,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/PrakBS21.dir/DependInfo.cmake"
+  "CMakeFiles/CLion.dir/DependInfo.cmake"
   )
